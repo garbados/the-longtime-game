fc1dda4ec82d4b5f95409c167b76de2a28c94f35
Diana Thayer <garbados@gmail.com>
2023-10-13 14:24:32 -0700

    checkin

--- src/the_longtime_game/core.clj
+++ src/the_longtime_game/core.clj
@@ -61,21 +61,20 @@
 
 (def infrastructure #{:todo}) ; TODO
 
-(def rationales #{:todo}) ; TODO
-
 (defn gen-name []
   (string/join "-" [(rand-nth first-syllable)
                     (rand-nth second-syllable)]))
-
-(s/fdef gen-name
-  :args (s/cat)
-  :ret ::name)
 
 (s/def ::name
   (s/with-gen string?
     #(g/fmap (partial string/join "-")
              (g/tuple (g/elements first-syllable)
                       (g/elements second-syllable)))))
+
+(s/fdef gen-name
+  :args (s/cat)
+  :ret ::name)
+
 (s/def ::age (s/int-in 0 100))
 (s/def ::passions (s/coll-of ::skill
                              :kind set?
@@ -103,24 +102,17 @@
 (s/def ::p ::nutrient)
 (s/def ::flora (s/int-in 0 5))
 
-(s/def ::meta
-  (s/or
-   :plains (s/keys :req-un [::infra
-                            ::n
-                            ::k
-                            ::p])
-   :forest (s/keys :req-un [::infra
-                            ::flora])
-   :steppe nil?
-   :basic  (s/keys :req-un [::infra])))
-
 (s/def ::location
   (s/and
-   (s/keys :req-un [::type
-                    ::meta])
-   (fn [location]
-     (contains? #{:basic (:type location)}
-                (first (:meta location))))))
+   (s/keys :req-un [::type])
+   (s/or
+    :plains (s/keys :req-un [::infra
+                             ::n
+                             ::k
+                             ::p])
+    :forest (s/keys :req-un [::infra
+                             ::flora])
+    :basic  (s/keys :req-un [::infra]))))
 
 (s/def ::ethos (s/coll-of ::skill :kind set? :count 2))
 (s/def ::syndicate ::ethos)
@@ -129,24 +121,22 @@
 (s/def ::individuals (s/coll-of ::individual))
 (s/def ::resource resources)
 (s/def ::stores (s/map-of ::resource pos-int?))
-(s/def ::rationale rationales)
-(s/def ::rationales (s/coll-of ::rationale :kind set?))
 (s/def ::locations (s/coll-of ::location))
 (s/def ::path (s/coll-of ::locations :kind vector?))
 (s/def ::index nat-int?)
+(s/def ::month nat-int?)
 (s/def ::herd
   (s/with-gen
     (s/keys :req-un [::individuals
                      ::syndicates
                      ::stores
-                     ::rationales
                      ::index
+                     ::month
                      ::path])
     #(g/fmap (fn [[individuals location]]
                {:individuals individuals
                 :syndicates #{}
                 :stores {}
-                :rationales #{}
                 :index 0
                 :path [[location]]})
              (g/tuple
@@ -194,25 +184,35 @@
   (let [{:keys [path]} herd]
     (assoc herd
            :index index
-           :path (conj (rest path)
-                       (first path)))))
+           :path (vec (conj (rest path)
+                            (first path))))))
 
 (s/fdef next-location
   :args (s/cat :herd ::herd
                :index ::index)
   :ret ::herd)
 
-(defn tally-votes [herd]
-  (->> (:individuals herd)
+(defn calculate-vote [individual skill]
+  (let [rank (get-in individual [:skills skill] 0)]
+    (if (contains? (:passions individual) skill)
+      (* rank 2)
+      rank)))
+
+(s/fdef calculate-vote
+  :args (s/cat :individual ::individual
+               :skill ::skill)
+  :ret nat-int?)
+
+(defn tally-votes [individuals]
+  (->> individuals
        (map
         (fn [individual]
           (reduce
-           (fn [all [skill rank]]
-             (assoc all skill (if (contains? (:passions individual) skill)
-                                (* rank 2)
-                                rank)))
+           (fn [all skill]
+             (assoc all skill
+                    (calculate-vote individual skill)))
            {}
-           (:skills individual))))
+           skills)))
        (reduce
         (fn [votes vote]
           (for [[skill value] votes]
@@ -221,12 +221,18 @@
           [skill 0]))
        (sort-by second >)))
 
+(s/fdef tally-votes
+  :args (s/cat :individuals ::individuals)
+  :ret (s/coll-of (s/tuple ::skill nat-int?)))
+
 (defn rank-candidates [votes]
   (let [leader (-> votes first first)]
     (if-let [runner (-> votes second first)]
-      (conj (lazy-seq
-             (rank-candidates (rest votes)))
-            #{leader runner})
+      (if (not= leader runner)
+        (conj (lazy-seq
+               (rank-candidates (rest votes)))
+              #{leader runner})
+        [])
       [])))
 
 (s/fdef rank-candidates
@@ -241,21 +247,94 @@
 (s/fdef select-candidate
   :args (s/cat :herd ::herd
                :candidates (s/coll-of ::syndicate))
-  :ret ::syndicate)
+  :ret (s/nilable ::syndicate))
 
-(defn add-syndicate [herd]
-  (let [votes (tally-votes herd)
-        candidates (rank-candidates votes)
-        candidate (select-candidate (:syndicates herd) candidates)]
-    (update herd :syndicates conj candidate)))
+(defn add-syndicate [{:keys [individuals syndicates] :as herd}]
+  (let [votes (tally-votes individuals)
+        candidates (rank-candidates votes)]
+    (if-let [candidate (select-candidate syndicates candidates)]
+      (update herd :syndicates conj candidate)
+      herd))) ; do not modify herd if there is no valid candidate
 
 (s/fdef add-syndicate
   :args (s/cat :herd ::herd)
   :ret ::herd)
 
 (defn remove-syndicate [herd]
-  (update herd :syndicates rest))
+  (update herd :syndicates (comp set rest)))
 
 (s/fdef remove-syndicate
   :args (s/cat :herd ::herd)
+  :ret ::herd)
+
+(defn inc-some-skill [skills*]
+  (let [skill (rand-nth (vec skills))
+        rank (get skills* skill 0)]
+    (if (= rank 5)
+      (inc-some-skill skills*)
+      (assoc skills* skill (inc rank)))))
+
+(s/fdef inc-some-skill
+  :args (s/cat :skills ::skills
+               :candidates (s/coll-of ::skill))
+  :ret ::skills)
+
+(defn gen-individual []
+  (let [age (+ 1 (rand-int 100))
+        base-skills (int (/ age 10))]
+    {:name (gen-name)
+     :age age
+     :fulfillment 50
+     :traits (set [(first (shuffle traits))])
+     :passions (set (take (+ 1 (rand-int 2))
+                          (shuffle (vec skills))))
+     :skills (reduce
+              (fn [skills _]
+                (inc-some-skill skills))
+              {}
+              (range base-skills))}))
+
+(s/fdef gen-individual
+  :args (s/cat)
+  :ret ::individual)
+
+(defn gen-individuals [n]
+  (for [_ (range n)]
+    (gen-individual)))
+
+(s/fdef gen-individuals
+  :args (s/cat :n (s/int-in 1 500))
+  :ret ::individuals)
+
+(defn gen-herd []
+  (let [individuals (gen-individuals 50)
+        path [[{:type :plains
+                :infra #{}
+                :n 2
+                :k 2
+                :p 2}]
+              [{:type :forest
+                :infra #{}
+                :flora 2}]
+              [{:type :mountain
+                :infra #{}}]
+              [{:type :steppe}]]]
+    (-> {:individuals individuals
+         :syndicates #{}
+         :path path
+         :stores (reduce
+                  (fn [all resource]
+                    (assoc all resource 50))
+                  {}
+                  resources)
+         :index 0
+         :month 0}
+        ;; add one syndicate by vote
+        add-syndicate
+        ;; and another by chance
+        (update :syndicates conj
+                (set (take 2 (shuffle (vec skills))))))))
+
+(s/fdef gen-herd
+  :args (s/cat)
   :ret ::herd)
--- test/the_longtime_game/core_test.clj
+++ test/the_longtime_game/core_test.clj
@@ -7,8 +7,20 @@
   (doseq [sym [`core/gen-name
                `core/death-chance
                `core/syndicate-name
-               `core/effective-skill]]
+               `core/effective-skill
+               `core/next-location
+               `core/calculate-vote
+               `core/tally-votes
+               `core/rank-candidates
+               `core/select-candidate
+               `core/add-syndicate
+               `core/remove-syndicate
+               `core/inc-some-skill
+               `core/gen-individual
+               `core/gen-individuals
+               `core/gen-herd
+               ]]
     (testing (str sym)
       (let [{:keys [failure]}
             (-> sym stest/check first stest/abbrev-result)]
-        (is (nil? failure) [sym failure])))))
+        (is (nil? failure) failure)))))
