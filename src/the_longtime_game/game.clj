0bda7d89ca715b8a7f1ed888a8faf207989830dc
Diana Thayer <garbados@gmail.com>
2023-11-09 08:14:01 -0800

    checkin lol

--- src/the_longtime_game/game.clj
+++ src/the_longtime_game/game.clj
@@ -43,24 +43,25 @@
 (defn new-game
   [& {:keys [forbidden]
       :or {forbidden #{}}}]
-  (println (text/wrap-quote-text
-            "Are you there, some enduring era
-             of peace and plenty?
-             Hard winters and bare summers
-             have weakened our ways
-             and scattered our herd.
-             Only a few dozen of us remain now to walk
-             the ancient path.
-             I believe in us, you must understand!
-             I believe in old age and young laughter,
-             in the strength of all that we might share.
-             I dream of homes among the stars,
-             of a long time for me, for us all.
-             Are you out there?
-             I believe in you.
-             I pray to you:
-             tell me your name."
-            :width text/default-width))
+  (println
+   (text/wrap-quote-text
+    "Are you there, some enduring era
+     of peace and plenty?
+     Hard winters and bare summers
+     have weakened our ways
+     and scattered our herd.
+     Only a few dozen of us remain now to walk
+     the ancient path.
+     I believe in us, you must understand!
+     I believe in old age and young laughter,
+     in the strength of all that we might share.
+     I dream of homes among the stars,
+     of a long time for me, for us all.
+     Are you out there?
+     I believe in you.
+     I pray to you:
+     tell me your name."
+    :width text/default-width))
   (let [spirit (repl/await-text nil "What shall the herd call you?" :forbidden forbidden :default "Longtime")
         herd (core/gen-herd :spirit spirit)]
     (println
@@ -88,19 +89,14 @@
 
 (defn launch-game
   []
-  (let [directory (io/file ".")
-        saves
-        (->> (file-seq directory)
-             (map
-              (fn [file]
-                (re-matches
-                 save-path-re
-                 (.getName file))))
-             (filter some?)
-             (map second))]
-    (if (seq saves)
-      (prompt-for-game saves)
-      (new-game))))
+  (if-let [saves
+           (->> (file-seq (io/file "."))
+                (map #(re-matches save-path-re (.getName %)))
+                (filter some?)
+                (map second)
+                seq)]
+    (prompt-for-game saves)
+    (new-game)))
 
 (defn game-loop
   [{:keys [spirit] :as herd}]
